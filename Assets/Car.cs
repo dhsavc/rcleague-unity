97c2ea8b35075d74de0ed692bc7550456d8ddcf4
Jacob Sommer <jacob.sommer@comcast.net>
2020-01-21 19:14:11 -0800

    Controller support

--- Assets/Car.cs
+++ Assets/Car.cs
@@ -1,30 +1,36 @@
 ﻿using UnityEngine;
 using UnityEngine.Networking;
+using UnityEngine.UI;
 using System;
 using System.Collections;
-using System.IO.Ports;
-using System.Threading;
 
 public class Car : MonoBehaviour
 {
 
-    string IP = "192.168.1.2";
+    string ip = "192.168.137.242";
     string port = "5000";
     private float prevVert = 0f;
     private float prevHoriz = 0f;
+    private bool sendingDrive= false;
+    private bool sendingTurn = false;
+    [SerializeField]
+    private InputField ipInput;
 
     // Update is called once per frame
     void Update()
     {
         float currentVert = Input.GetAxis("Vertical");
-        if (currentVert != prevVert) {
+        if (currentVert != prevVert && !sendingDrive)
+        {
             prevVert = currentVert;
+            sendingDrive = true;
             StartCoroutine(move("drive", currentVert));
         }
         float currentHoriz = Input.GetAxis("Horizontal");
-        if (currentHoriz != prevHoriz)
+        if (currentHoriz != prevHoriz && !sendingTurn)
         {
             prevHoriz = currentHoriz;
+            sendingTurn = true;
             StartCoroutine(move("turn", currentHoriz));
         }
 
@@ -34,13 +40,27 @@ public class Car : MonoBehaviour
     {
         WWWForm form = new WWWForm();
         form.AddField("value", value + "");
-        UnityWebRequest www = UnityWebRequest.Post("http://" + IP + ":" + port + "/" + type, form);
+        UnityWebRequest www = UnityWebRequest.Post("http://" + ip + ":" + port + "/" + type, form);
         yield return www.SendWebRequest();
+
+        if (type.Equals("drive"))
+        {
+            sendingDrive = false;
+        }
+        else
+        {
+            sendingTurn = false;
+        }
 
         if (www.isNetworkError || www.isHttpError)
         {
             Debug.Log(www.error);
         }
+    }
+
+    public void updateIp()
+    {
+        this.ip = ipInput.text; 
     }
 
 }
